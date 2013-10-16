41636cddf225b15bc9617d4f84515a183341c228
samriggs <samriggs>
2013-10-16 02:00:50 -0400

    wrap core of quiz2 into function

--- biHomeworks/chapter1/quiz2_solution.py
+++ biHomeworks/chapter1/quiz2_solution.py
@@ -8,15 +8,18 @@ CODE CHALLENGE: Solve the Reverse Complement Problem.
 from bi_utils.helpers import sane_open
 from cStringIO import StringIO
 
-with sane_open("stepic_dataset.txt") as f:
-    # [::-1] is a way to use string slicing syntax to reverse strings.
-    rev_seq = f.readline()[::-1]
-    # now complement.
-    comp_rev_seq = StringIO()
-    for i in range(1, len(rev_seq)):
-        if rev_seq[i] is "A": comp_rev_seq.write("T")
-        elif rev_seq[i] is "C": comp_rev_seq.write("G")
-        elif rev_seq[i] is "G": comp_rev_seq.write("C")
-        elif rev_seq[i] is "T": comp_rev_seq.write("A")
-    print comp_rev_seq.getvalue()
+def rev_comp():
+    with sane_open("stepic_dataset.txt") as f:
+        # [::-1] is a way to use string slicing syntax to reverse strings.
+        rev_seq = f.readline()[::-1]
+        # now complement.
+        comp_rev_seq = StringIO()
+        for i in range(1, len(rev_seq)):
+            if rev_seq[i] is "A": comp_rev_seq.write("T")
+            elif rev_seq[i] is "C": comp_rev_seq.write("G")
+            elif rev_seq[i] is "G": comp_rev_seq.write("C")
+            elif rev_seq[i] is "T": comp_rev_seq.write("A")
+        return comp_rev_seq.getvalue()
+    
+print rev_comp()
     
